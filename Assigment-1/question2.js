bc090e64d62cc2e479cc2c49bdfc03aed94a1391
Niranjan Rai <rainiranjan1998@gmail.com>
2023-05-30 14:49:38 +0530

    first commit

---
+++ Assigment-1/question2.js
@@ -0,0 +1,18 @@
+function removeElement(nums, val) {
+  let i = 0;
+
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] !== val) {
+      nums[i] = nums[j];
+      i++;
+    }
+  }
+
+  return i;
+}
+
+const nums = [3, 2, 2, 3];
+const val = 3;
+const result = removeElement(nums, val);
+console.log(result); // Output: 2
+console.log(nums); // Output: [2, 2, ...]
